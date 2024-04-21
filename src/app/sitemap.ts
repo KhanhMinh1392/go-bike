b052fbd88a6352b1c2965749c35744d678a8df3a
KhanhMinh1392 <81310329+KhanhMinh1392@users.noreply.github.com>
2024-04-21 12:22:05 +0700

    fix(seo): cannot reach file robot.txt

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,6 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const languageEntries: MetadataRoute.Sitemap = locales.map((locale) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/${locale}`,
   }));
+  
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
